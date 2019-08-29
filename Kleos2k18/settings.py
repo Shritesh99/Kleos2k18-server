09cf87215ba64428a9654f66fd055fc2030aa811
shritesh99 <shritesh.sj@gmail.com>
2019-08-30 03:40:59 +0530

    changed static root

--- Kleos2k18/settings.py
+++ Kleos2k18/settings.py
@@ -138,7 +138,7 @@ SESSION_COOKIE_AGE = 1209600
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
